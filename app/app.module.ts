47a19ec116637e019f470c0411b6155268df564e
sydney <sydgael@gmail.com>
2016-12-27 22:26:18 +0100

    Angular 2 start project with ng2-bootstrap (alert example)

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }   from './app.component';
+import {AlertModule} from "ng2-bootstrap";
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, AlertModule.forRoot() ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })

f9aff78822e2f6aea870564d2c58f42c96d2e1aa
ADJOU MOUMOUNI Sydney <sydgael@gmail.com>
2016-12-27 22:30:27 +0100

    Update app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: '<h1>hello word</h1>'
+  templateUrl: 'app/basic.html'
 })
 export class AppComponent { }
 
